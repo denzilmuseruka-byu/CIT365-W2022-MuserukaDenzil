78fd6b25bff22c6b2752d61fe9cddce9567d8f81
denzilmuseruka-byu <denzilmuseruka@byui.edu>
2022-01-08 03:43:47 +0200

    Week 01

---
+++ MyFirstConsoleApplication/GlazerApp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstConsoleApplication
+{
+    public static class GlazerApp
+    {
+        public static void RunExample()
+        {
+            double width, height, woodLength, glassArea;
+            string widthString, heightString;
+            // prompt for width
+            Console.Write("Enter width: ");
+            widthString = Console.ReadLine();
+            width = double.Parse(widthString);
+
+            // prompt for height
+            Console.Write("Enter height: ");
+            heightString = Console.ReadLine();
+            height = double.Parse(heightString);
+            
+            woodLength = 2 * (width + height) * 3.25;
+            glassArea = 2 * (width * height);
+            Console.WriteLine("The length of the wood is " +
+            woodLength + " feet");
+            Console.WriteLine("The area of the glass is " +
+            glassArea + " square metres");
+        }
+    }
+}
---
+++ MyFirstConsoleApplication/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetUserNameAndLocation();
+            ChristmasCountdown(DateTime.Now);
+            GlazerApp.RunExample();
+
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+        }
+        private static void GetUserNameAndLocation()
+        {
+            Person person = new Person();
+            // prompt for the name
+            Console.WriteLine("What is your name? ");
+            person.name = Console.ReadLine();
+
+            // prompt for the user's location
+            Console.WriteLine($"Hi {person.name}! Where are you from? ");
+            person.location = Console.ReadLine();
+
+            Console.WriteLine($"I have never been to {person.location}.I bet it is nice.Press any key to continue...");
+            Console.ReadKey();
+        }
+        private static void ChristmasCountdown(DateTime date)
+        {
+            // showing today's date
+            Console.WriteLine($"Today's date is: {DateTime.Now.ToString("MM/dd/yyyy")}");
+
+            // calculate days before christmas
+            var daysTillChristmas = ( DateTime.Parse("2022/12/25")- date).Days;
+
+            // display days before christmas
+            Console.WriteLine($"There are {daysTillChristmas} days until Christmas!");
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+        }
+    }
+}
